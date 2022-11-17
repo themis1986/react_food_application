e658fd7b14c50734d7d2d0e3792021bf2f43fec2
thiakovidis <thiakovidis@agroapps.gr>
2022-11-17 15:49:36 +0200

    Open/Close modal functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCardButton></HeaderCardButton>
+        <HeaderCardButton onClick={props.onShowCart}></HeaderCardButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicius food!" />
